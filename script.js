f61e471663adc1fd73f02f02601d651a827f8c72
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-01 20:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,30 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-	let str=""
-	while(num!=0){
-		if(num>1000){
-			str+=M;
-			num-=1000;
-		}else if(num>500){
-			str+=D
-			num-=500;
-		}else if(num>100){
-			str+=C
-			num-=100;
-		}else if(num>50){
-			str+=L
-			num-=50;
-		}else if(num>10){
-			str+=X;
-			num-=10;
-		}else if(num>5){
-			str+=V;
-			num-=5;
-		}else{
-			if(num==4){
-				str+=IX;
-				num-=4;
-			}else if(num==3){
-				str+=III;
-				num-=3;
-			}else if(num==2){
-				str+=II;
-				num-=2;
-			}else{
-				str+=I;
-				num-=1;
-			}
-		}
-	}
-	return str;
+  	 const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+
+    let str = '';
+
+    for (let [roman, value] of romanNumerals) {
+        while (num >= value) {
+            str += roman;
+            num -= value;
+        }
+    }
+
+    return str;
 
   //your code here
 
